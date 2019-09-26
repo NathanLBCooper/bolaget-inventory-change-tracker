98f3bef16ba5879316454d0082c45eaca5c8e548
NathanLBCooper <nathan.lb.cooper@outlook.com>
2019-09-26 17:44:59 +0200

    build web on appveyor

---
+++ build.ps1
@@ -0,0 +1,22 @@
+$LASTEXITCODE = 0
+
+$build_dir = ".\build"
+$expo_web_output_dir = ".\app\web-build"
+$expo_web_output = $expo_web_output_dir + "\*"
+
+if(!$LASTEXITCODE -and (Test-Path $build_dir))
+{
+    Remove-Item $build_dir -Recurse -Force
+}
+if(!$LASTEXITCODE -and (Test-Path $expo_web_output_dir))
+{
+    Remove-Item $expo_web_output_dir -Recurse -Force
+}
+
+if(!$LASTEXITCODE) { Invoke-Expression -Command: "cd app; expo build:web; cd.." }
+
+if(!$LASTEXITCODE) { mkdir -path $build_dir }
+if(!$LASTEXITCODE) { Copy-Item -Path $expo_web_output -Destination $build_dir -Recurse }
+if(!$LASTEXITCODE) { Remove-Item $expo_web_output_dir -Recurse -Force }
+
+exit $LASTEXITCODE
