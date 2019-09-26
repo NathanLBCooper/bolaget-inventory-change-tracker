df7d71d106d536f6533d76566483d42702ac973d
Nathan Cooper <nathan.lb.cooper@outlook.com>
2019-09-26 20:16:25 +0200

    Remove error handling. It doesn't work and I'm not fixing it

--- build.ps1
+++ build.ps1
@@ -1,14 +1,12 @@
-$LASTEXITCODE = 0
-
 $build_dir = ".\build"
 $expo_web_output_dir = ".\app\web-build"
 $expo_web_output = $expo_web_output_dir + "\*"
 
-if(!$LASTEXITCODE -and (Test-Path $build_dir))
+if(Test-Path $build_dir)
 {
     Remove-Item $build_dir -Recurse -Force
 }
-if(!$LASTEXITCODE -and (Test-Path $expo_web_output_dir))
+if(Test-Path $expo_web_output_dir)
 {
     Remove-Item $expo_web_output_dir -Recurse -Force
 }
@@ -16,10 +14,8 @@ if(!$LASTEXITCODE -and (Test-Path $expo_web_output_dir))
 Rename-Item -Path ".\app\appsettings.json" -NewName "appsettings.dev.json"
 Rename-Item -Path ".\app\appsettings.prod.json" -NewName "appsettings.json"
 
-if(!$LASTEXITCODE) { Invoke-Expression -Command: "cd app; expo build:web --no-pwa; cd.." }
+Invoke-Expression -Command: "cd app; expo build:web --no-pwa; cd.."
 
-if(!$LASTEXITCODE) { mkdir -path $build_dir }
-if(!$LASTEXITCODE) { Copy-Item -Path $expo_web_output -Destination $build_dir -Recurse }
-if(!$LASTEXITCODE) { Remove-Item $expo_web_output_dir -Recurse -Force }
-
-exit $LASTEXITCODE
+mkdir -path $build_dir
+Copy-Item -Path $expo_web_output -Destination $build_dir -Recurse
+Remove-Item $expo_web_output_dir -Recurse -Force
