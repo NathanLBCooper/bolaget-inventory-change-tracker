2b79b1f1c12421b8add4f3da086c173ea51de24c
Nathan Cooper <nathan.lb.cooper@outlook.com>
2019-09-27 01:03:43 +0200

    Replace splash screen, use css shapes rather than images

--- app/src/screens/SplashScreen.tsx
+++ app/src/screens/SplashScreen.tsx
@@ -1,12 +1,63 @@
-import React from 'react';
-import { Text, View, Image, StyleSheet } from 'react-native';
+import React from "react";
+import { Text, View, StyleSheet } from 'react-native';
 
 const styles: any = StyleSheet.create({
   container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center'
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "lightslategrey"
   },
+  textBlock: {
+    alignItems: "center"
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold"
+  },
+  subtitle: {
+    fontSize: 20
+  }
+});
+
+const hexagonStyles: any = StyleSheet.create({
+  hexagon: {
+    width: 300,
+    height: 165
+  },
+  hexagonInner: {
+    width: 300,
+    height: 165,
+    backgroundColor: '#fae1e6'
+  },
+  hexagonAfter: {
+    position: 'absolute',
+    bottom: -75,
+    left: 0,
+    width: 0,
+    height: 0,
+    borderStyle: 'solid',
+    borderLeftWidth: 150,
+    borderLeftColor: 'transparent',
+    borderRightWidth: 150,
+    borderRightColor: 'transparent',
+    borderTopWidth: 75,
+    borderTopColor: '#fae1e6'
+  },
+  hexagonBefore: {
+    position: 'absolute',
+    top: -75,
+    left: 0,
+    width: 0,
+    height: 0,
+    borderStyle: 'solid',
+    borderLeftWidth: 150,
+    borderLeftColor: 'transparent',
+    borderRightWidth: 150,
+    borderRightColor: 'transparent',
+    borderBottomWidth: 75,
+    borderBottomColor: '#fae1e6'
+  }
 });
 
 export class SplashScreen extends React.Component<{
@@ -14,8 +65,17 @@ export class SplashScreen extends React.Component<{
 }> {
   public render(): React.ReactNode {
     return (<View style={styles.container}>
-      <Image source={require("../../images/wine.png")} style={{ width: 200, height: 200 }} />
-      <Text style={{ fontSize: 30 }}>Secrets of Systemet</Text>
+      <View style={hexagonStyles.hexagon}>
+        <View style={hexagonStyles.hexagonInner}>
+          <View style={styles.textBlock}>
+            <Text style={styles.title}>BOLAGET</Text>
+            <Text>+/-</Text>
+            <Text style={styles.subtitle}>Inventory Change Tracker</Text>
+          </View>
+        </View>
+        <View style={hexagonStyles.hexagonBefore} />
+        <View style={hexagonStyles.hexagonAfter} />
+      </View>
     </View>);
   }
   public componentDidMount(): void {
