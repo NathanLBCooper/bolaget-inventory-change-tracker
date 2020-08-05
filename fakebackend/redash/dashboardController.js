483bbcee290ce7d25e3f4811e689bb0f737ab292
Nathan Cooper <nathan.lb.cooper@outlook.com>
2020-08-05 21:14:40 +0200

    Use "better" dashboard, and hook fakebackend up to the real file

--- fakebackend/redash/dashboardController.js
+++ fakebackend/redash/dashboardController.js
@@ -1,15 +1,9 @@
+var dashboards = require("../../redash/dashboards.json");
+
 module.exports = function (app) {
     app.get("/dashboards", function (req, res) {
         res.send({
-            data: dashboards
+            data: dashboards.data
         });
     });
 };
-
-const dashboards = [
-    {
-        name: "inventory-change-tracker",
-        uri: "http://40.69.83.250/public/dashboards/OeLpkqJ8fcLgoLq44Ciq59KyohVISWhQhdMfpRW5?org_slug=default",
-        description: "This is a dashboard I quickly made so I'd have something to work with"
-    }
-];
